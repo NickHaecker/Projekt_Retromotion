0bb1ef02c98952c664dc0b988dc74f45f54b8134
NickHaecker <56677688+NickHaecker@users.noreply.github.com>
2019-11-28 22:01:01 +0100

    Footer und App-Bar
    Footer und App-Bar

---
+++ retromotion/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+// import vorfooter from "../components/vorfooter";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "",
+    name: "Home",
+    component: Home
+  },
+  // {
+  //   path: "/",
+  //   name: "VorFooter",
+  //   component: vorfooter
+  // },
+
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
